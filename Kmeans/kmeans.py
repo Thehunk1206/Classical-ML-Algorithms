dfa84fb47ac3c21a1dd1c95e440e6f7028cf983d
thehunk1206 <khantauhid313@gmail.com>
2021-12-29 00:24:12 +0530

    Added 3D plot for kmeans clustering

--- Kmeans/kmeans.py
+++ Kmeans/kmeans.py
@@ -1,6 +1,7 @@
 from typing import List
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 from sklearn.datasets import make_blobs, make_classification, make_swiss_roll, make_moons  
 
 import sys
@@ -12,6 +13,8 @@ class KMeansClustering(object):
         self.K = K
         self.max_iter = max_iter
         self.num_datapoints, self.num_feat = X.shape
+        self.fitted_centroids = None
+        self.inertia = 0
 
     def init_centroids(self, X:np.ndarray) -> np.ndarray:
         # centroids = np.zeros(shape=(self.K, self.num_feat))
@@ -56,12 +59,21 @@ class KMeansClustering(object):
         plt.scatter(centroids[:,0], centroids[:,1], c='black', s=100, alpha=0.7, marker='x')
         plt.show()
     
+    def plot_3d_cluster(self, centroids, x, y):
+        ax = plt.axes(projection='3d')
+        ax.scatter3D(x[:,0], x[:,1], x[:,2], c=y, s=20, cmap='viridis')
+        ax.scatter3D(centroids[:,0], centroids[:,1], centroids[:,2], c='black', s=100, alpha=1.0, marker='o')
+        plt.show()
+
     def get_y_label(self, clusters:List[list], X:np.ndarray):
         y_label = np.zeros(shape=(self.num_datapoints))
         for idx, cluster in enumerate(clusters):
             for point_idx in cluster:
                 y_label[point_idx] = idx
         return y_label
+
+    def predict(self, X:np.ndarray):
+        pass
 
     def fit(self, X:np.ndarray):
         centroids = self.init_centroids(X)
@@ -75,21 +87,26 @@ class KMeansClustering(object):
             if diff.any() < 0.0001:
                 break
 
+        self.fitted_centroids_ = centroids
 
         y_label = self.get_y_label(clusters, X)
 
         if self.num_feat == 2:
             self.plot_cluster(centroids,X, y_label)
+        elif self.num_feat == 3:
+            self.plot_3d_cluster(centroids, X, y_label)
         
         return y_label
         
 
 if __name__ == "__main__":
     np.random.seed(3)
-    K = 2
-    # X, _ = make_blobs(n_samples=1000, centers=K, n_features=2, cluster_std=1.0, random_state=1)
-    # X, _ = make_classification(n_samples=1000, n_features=2, n_redundant=0, n_informative=2, n_classes=K, n_clusters_per_class=1)
-    X, _ = make_moons(n_samples=1000, noise=0.1)
+    K = 4
+    num_of_features = 2
+    num_of_samples = 1000
+    X, _ = make_blobs(n_samples=num_of_samples, centers=K, n_features=num_of_features, cluster_std=1.0, random_state=1)
+    # X, _ = make_classification(n_samples=num_of_samples, n_features=num_of_features, n_redundant=0, n_informative=2, n_classes=K, n_clusters_per_class=1)
+    # X, _ = make_moons(n_samples=num_of_features, noise=0.1)
 
     kmeans = KMeansClustering(X, K, max_iter=30)
     y_label = kmeans.fit(X)
---
+++ PCA/PCA_wCOV.py
@@ -0,0 +1,1 @@
+# class to perform PCA on a dataset with covariance matrix
